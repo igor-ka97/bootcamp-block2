58c63462f4af2ff8b097e8240a302aeb31b97bb5
igor-ka97 <68326604+igor-ka97@users.noreply.github.com>
2020-07-21 16:14:44 +0700

    BootCamp

--- template/assets/js/counter.js
+++ template/assets/js/counter.js
@@ -1,14 +1,24 @@
 
 $( document ).ready(function() {
-    $('.counter__item').on('click', countInput);
+    $('.counter__item').click(countInput);
+    $('.counter__input').change(changeInput);
+
     function countInput() {
         $input = $(this).closest('.car__quantity').find('.counter__input');
-        if ($(this).hasClass("counter__item-plus")) {
+        $action = $(this).data('count');
+        if ($action == 'plus') {
             $input.val(parseInt($input.val()) + 1);
-        } else if ($(this).hasClass("counter__item-minus")) {
+        } else if ($action == 'minus') {
             if (parseInt($input.val()) > 1) {
                 $input.val(parseInt($input.val()) - 1);
             }
+        }
+    }
+    function changeInput() {
+        $input = $(this).closest('.car__quantity').find('.counter__input');
+        $inputValue = $input.val();
+        if (parseInt($inputValue) < 1 || $inputValue == '') {
+            $input.val(1);
         }
     }
 });
